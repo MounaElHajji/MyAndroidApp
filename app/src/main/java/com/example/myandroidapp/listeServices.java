ae7cb817ea42e93b58cf95fdde564d387b6b9ee2
Nouhaila Ziyane <nouhaila.ziyane@uit.ac.ma>
2022-11-11 23:25:26 +0100

    Ajout de la liste des services

--- app/src/main/java/com/example/myandroidapp/listeServices.java
+++ app/src/main/java/com/example/myandroidapp/listeServices.java
@@ -23,6 +23,12 @@ public class listeServices extends AppCompatActivity {
         ButterKnife.bind(this);
         ArrayList<Service> services= new ArrayList<Service>();
         services.add(new Service("Plomberie",R.drawable.pl));
+        services.add(new Service("Electricité",R.drawable.ele));
+        services.add(new Service("Peinture",R.drawable.pei));
+        services.add(new Service("Climatisation",R.drawable.cli));
+        services.add(new Service("Bricolage",R.drawable.bri));
+        services.add(new Service("Femme de ménage",R.drawable.fe));
+
         ServiceAdapter serviceAdapter= new ServiceAdapter(getApplicationContext(), R.layout.activity_liste_services, services);
         listServ.setAdapter(serviceAdapter);
     }
