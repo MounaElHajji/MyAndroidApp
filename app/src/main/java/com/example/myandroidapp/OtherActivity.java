d1091d672d4849b268a133c06f2043f5101078c5
oumami maryam <oumamimaryam49@gmail.com>
2022-11-12 00:34:25 +0100

    page_inscription

--- app/src/main/java/com/example/myandroidapp/OtherActivity.java
+++ app/src/main/java/com/example/myandroidapp/OtherActivity.java
@@ -1,14 +1,20 @@
 package com.example.myandroidapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class OtherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.otheractivity);
+
+
+
+
+
+
     }
+
 }
