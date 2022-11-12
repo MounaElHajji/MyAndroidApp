f9a316dde57b4a97a96ad616b8400a2692f947e0
Mouna <montalina833@gmail.com>
2022-11-12 08:54:36 +0000

    autreInfo commit

--- app/src/main/java/com/example/myandroidapp/EmployeelistActivity.java
+++ app/src/main/java/com/example/myandroidapp/EmployeelistActivity.java
@@ -5,16 +5,50 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.example.myandroidapp.Adapters.EmployeeAdapter;
+import com.example.myandroidapp.Models.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeelistActivity extends AppCompatActivity {
 
 
     private RecyclerView recyclerViewVar;
 
+    List<Employee> EmployeeList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.employee_list_activity);
 
+        Spinner myspinner = findViewById(R.id.spinner1);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(EmployeelistActivity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.filtrage));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        myspinner.setAdapter(myAdapter);
+
+        recyclerViewVar = findViewById(R.id.recycleView);
+        recyclerViewVar.setHasFixedSize(true);
+        recyclerViewVar.setLayoutManager(new LinearLayoutManager(this));
+
+        EmployeeAdapter postAdapter = new EmployeeAdapter(EmployeelistActivity.this, getData());
+        recyclerViewVar.setAdapter(postAdapter);
+    }
+
+    public static List<Employee> getData() {
+        List<Employee> data = new ArrayList<>();
+        String[] ids = {"310", "313", "320"};
+        String[] noms = {"EL Hajji", "EL Badaoui", "Raki"};
+        String[] villes = {"Kenitra", "Rabat", "Casa"};
+        String[] descriptions = {"Plombier", "Etudiante", "name of three"};
+
+        for (int i=0; i<ids.length; i++) {
+            data.add(new Employee(ids[i], noms[i], villes[i], descriptions[i]));
+        }
+        return data;
     }
 }
--- app/src/main/java/com/example/myandroidapp/Models/Employee.java
+++ app/src/main/java/com/example/myandroidapp/Models/Employee.java
@@ -1,4 +1,101 @@
 package com.example.myandroidapp.Models;
 
 public class Employee {
+    private String id, ville, nom, description, image, prenom, rue, codeP, tel, password;
+    private String rating;
+
+    public Employee(String id, String ville, String nom, String description) {
+        this.id= id;
+        this.ville = ville;
+        this.nom = nom;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getRue() {
+        return rue;
+    }
+
+    public void setRue(String rue) {
+        this.rue = rue;
+    }
+
+    public String getCodeP() {
+        return codeP;
+    }
+
+    public void setCodeP(String codeP) {
+        this.codeP = codeP;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
 }
