a95ecb943e88249f8d7b2e55d72e3d1a11fd2d5e
Kawtar Bekkali <kawtarbekkali1@gmail.com>
2022-11-13 14:49:24 +0100

    Page des paramètres màj

--- app/src/main/java/com/example/myandroidapp/models/Settings.java
+++ app/src/main/java/com/example/myandroidapp/models/Settings.java
@@ -1,12 +1,10 @@
 package com.example.myandroidapp.models;
 
-import com.example.myandroidapp.models.Footer;
 import com.example.myandroidapp.MainActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
