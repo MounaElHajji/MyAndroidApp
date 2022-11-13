d4fa1ebe62bc736e533b1293bba732dbb9a8a9a1
Aouzaloumaima <oumaima.aouzal@uit.ac.ma>
2022-11-13 14:57:31 +0100

    commitPageacceuil

--- app/src/main/java/com/example/myandroidapp/MainActivity.java
+++ app/src/main/java/com/example/myandroidapp/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.myandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+    public void openActivity()
+    {
+        Intent inten = new Intent(this,MainActivity2.class);
+        startActivity(inten);
+    }
+    @OnClick(R.id.button)
+    protected void buttonClick(){
+
+        openActivity();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
