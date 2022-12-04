fae3b5f0d351ba49e0720c9da33dcb34eff143f7
oumami maryam <oumamimaryam49@gmail.com>
2022-12-04 20:06:12 +0100

    Last-changes

--- app/src/main/java/com/example/myandroidapp/retrofit/RetrofitClient.java
+++ app/src/main/java/com/example/myandroidapp/retrofit/RetrofitClient.java
@@ -1,13 +1,12 @@
 package com.example.myandroidapp.retrofit;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "http://192.168.1.27:8080/";
+    private static final String BASE_URL = "http://192.168.1.102:8080/";
 
     public static Retrofit getRetrofitInstance(){
         if(retrofit == null){
