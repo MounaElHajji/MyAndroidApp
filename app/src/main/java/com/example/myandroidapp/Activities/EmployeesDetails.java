bec41ecb7427679ea14dd9ab18c4a557b419dadc
Mouna <montalina833@gmail.com>
2022-12-28 19:33:29 +0000

    emlployee rating

--- app/src/main/java/com/example/myandroidapp/Activities/EmployeesDetails.java
+++ app/src/main/java/com/example/myandroidapp/Activities/EmployeesDetails.java
@@ -3,44 +3,273 @@ package com.example.myandroidapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import com.example.myandroidapp.Api.AccountApi;
+import com.example.myandroidapp.Api.ApiInterface;
+import com.example.myandroidapp.Models.Employee;
+import com.example.myandroidapp.Models.Rating;
 import com.example.myandroidapp.R;
+import com.example.myandroidapp.retrofit.RetrofitClient;
+import com.example.myandroidapp.retrofit.RetrofitS;
+
+import java.util.List;
 
 public class EmployeesDetails extends AppCompatActivity {
 
+    List<Rating> ratingList;
+    RatingBar ratingBar, ratingBarTotal;
+    TextView villeTxt, nomTxt, adressTxt, emploiTxt, descTxt, telTxt, employeeDeatls, responseTV, averageRating;
+    String nom;
+    String ville;
+    String description;
+    String image;
+    String telephone;
+    String emploie;
+    String rating;
+    String id;
+    String typeProfile;
+    Button buttonRat;
+    AccountApi api1;
+    float myRating = 0;
+    ApiInterface apiInterface;
+    SharedPreferences sh;
+    RetrofitS retrofitS= new RetrofitS();
+    AccountApi api=retrofitS.getRetrofit().create(AccountApi.class);
 
-    TextView villeTxt, nomTxt, adressTxt, emploiTxt, descTxt, telTxt;
-    String nom, ville, description, image, telephone, emploie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Employee emp = new Employee();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employees_details);
 
+        sh = getSharedPreferences("MySharedPref", Context.MODE_PRIVATE);
         villeTxt = findViewById(R.id.textView4);
         nomTxt = findViewById(R.id.textView);
         adressTxt = findViewById(R.id.textView8);
         emploiTxt = findViewById(R.id.textView10);
         descTxt = findViewById(R.id.textView12);
         telTxt = findViewById(R.id.textView6);
+        ratingBar = findViewById(R.id.ratingBar);
+        buttonRat = findViewById(R.id.buttonRat);
+        responseTV = findViewById(R.id.idTVResponse);
+        employeeDeatls = findViewById(R.id.idTest);
+        averageRating = findViewById(R.id.rating);
+        ratingBarTotal = findViewById(R.id.ratingBar2);
 
         Intent intent = getIntent();
-
         nom = intent.getStringExtra("firstName");
         ville = intent.getStringExtra("city");
         description = intent.getStringExtra("description");
         image = intent.getStringExtra("imageP");
         telephone = intent.getStringExtra("tel");
         emploie = intent.getStringExtra("typeProfil");
+        id = intent.getStringExtra("id");
+        typeProfile = intent.getStringExtra("typeProfil");
+
+
+
+        SumRating();
+
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                int rating = (int) v;
+                String message = null;
+
+                myRating = ratingBar.getRating();
+                ratingBar.setRating(myRating);
+                switch (rating) {
+                    case 1:
+                        message = "Sorry to hear that! :(";
+                        break;
+
+                    case 2:
+                        message = "You always accept suggestions";
+                        break;
+
+                    case 3:
+                        message = "Good enough";
+                        break;
+
+                    case 4:
+                        message = "Great! Thank you";
+                        break;
+
+                    case 5:
+                        message = "Awesome";
+                        break;
+                }
+
+                Toast.makeText(EmployeesDetails.this, message, Toast.LENGTH_SHORT).show();
+            }
+        });
+        int id_current= sh.getInt("id",0);
+
+
+
+        ratingBar.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    float touchPositionX = event.getX();
+                    float width = ratingBar.getWidth();
+                    float starsf = (touchPositionX / width) * 5.0f;
+                    int stars = (int)starsf + 1;
+                    if(!ratingList.contains(id))
+                    {
+                        RateEmplployee(String.valueOf(stars));
+                    }
+                    Toast.makeText(EmployeesDetails.this, String.valueOf("test"), Toast.LENGTH_SHORT).show();
+                    v.setPressed(false);
+//                    UpdateRating(String.valueOf(stars));
+                }
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    v.setPressed(true);
+                }
+
+                if (event.getAction() == MotionEvent.ACTION_CANCEL) {
+                    v.setPressed(false);
+                }
+
+                return false;
+            }
+        });
+
+
 
         nomTxt.setText(nom);
         villeTxt.setText(ville);
         descTxt.setText(description);
         emploiTxt.setText(emploie);
         telTxt.setText(telephone);
+        employeeDeatls.setText(id);
+    }
 
+    private void UpdateRating(String label) {
+        Rating rating = new Rating(label);
 
+        apiInterface = RetrofitClient.getRetrofitInstance().create(ApiInterface.class);
+        // calling a method to create an update and passing our modal class.
+        Call<Employee> call = apiInterface.updateRating(id, 60, rating);
+
+        call.enqueue(new Callback<Employee>() {
+            @Override
+            public void onResponse(Call<Employee> call, Response<Employee> response) {
+                // this method is called when we get response from our api.
+                Toast.makeText(EmployeesDetails.this, "Data updated to API", Toast.LENGTH_SHORT).show();
+                // on below line we are setting empty
+                // text to our both edit text.
+
+                // we are getting a response from our body and
+                // passing it to our modal class.
+                Employee responseFromAPI = response.body();
+
+                // on below line we are getting our data from modal class
+                // and adding it to our string.
+                String responseString = "Response Code : " + response.code() + "\nName : " + responseFromAPI.getRating();
+
+                // below line we are setting our string to our text view.
+                responseTV.setText(responseString);
+            }
+
+            @Override
+            public void onFailure(Call<Employee> call, Throwable t) {
+
+                // setting text to our text view when
+                // we get error response from API.
+                responseTV.setText("Error found is : " + t.getMessage());
+            }
+        });
+    }
+
+    private void SumRating()
+    {
+        apiInterface = RetrofitClient.getRetrofitInstance().create(ApiInterface.class);
+        // calling a method to create an update and passing our modal class.
+        Call<Float> call = apiInterface.SumRating(id);
+
+        call.enqueue(new Callback<Float>() {
+            @Override
+            public void onResponse(Call<Float> call, Response<Float> response) {
+                // this method is called when we get response from our api.
+                Toast.makeText(EmployeesDetails.this, "Data updated to API", Toast.LENGTH_SHORT).show();
+                // on below line we are setting empty
+                // text to our both edit text.
+
+                // we are getting a response from our body and
+                // passing it to our modal class.
+                Float responseFromAPI = response.body();
+
+                // on below line we are getting our data from modal class
+                // and adding it to our string.
+
+                // below line we are setting our string to our text view.
+                String ratText = Float.toString(responseFromAPI);
+
+                ratingBarTotal.setRating(responseFromAPI);
+
+                averageRating.setText(ratText);
+            }
+
+            @Override
+            public void onFailure(Call<Float> call, Throwable t) {
+
+                // setting text to our text view when
+                // we get error response from API.
+                averageRating.setText("Error found is : " + t.getMessage());
+            }
+        });
+    }
+
+    private void RateEmplployee(String label) {
+        Rating rating = new Rating(label);
+
+        apiInterface = RetrofitClient.getRetrofitInstance().create(ApiInterface.class);
+        // calling a method to create an update and passing our modal class.
+        Call<Employee> call = apiInterface.AddRating(rating, id);
+
+        call.enqueue(new Callback<Employee>() {
+            @Override
+            public void onResponse(Call<Employee> call, Response<Employee> response) {
+                // this method is called when we get response from our api.
+                Toast.makeText(EmployeesDetails.this, "Data updated to API", Toast.LENGTH_SHORT).show();
+                // on below line we are setting empty
+                // text to our both edit text.
+
+                // we are getting a response from our body and
+                // passing it to our modal class.
+                Employee responseFromAPI = response.body();
+
+                // on below line we are getting our data from modal class
+                // and adding it to our string.
+                String responseString = "Response Code : " + response.code() + "\nName : " + responseFromAPI.getRating();
+
+                // below line we are setting our string to our text view.
+                responseTV.setText(responseString);
+            }
+
+            @Override
+            public void onFailure(Call<Employee> call, Throwable t) {
+
+                // setting text to our text view when
+                // we get error response from API.
+                responseTV.setText("Error found is : " + t.getMessage());
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myandroidapp/Api/ApiInterface.java
+++ app/src/main/java/com/example/myandroidapp/Api/ApiInterface.java
@@ -4,15 +4,18 @@ import com.example.myandroidapp.Models.Account;
 import com.example.myandroidapp.Models.Employee;
 import com.example.myandroidapp.Models.ListFavoris;
 import com.example.myandroidapp.Models.Person;
+import com.example.myandroidapp.Models.Rating;
 import com.example.myandroidapp.Models.Ville;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Part;
 import retrofit2.http.Path;
 
@@ -52,7 +55,7 @@ public interface ApiInterface {
 
     @Multipart
     @POST("/favList/deleteFav")
-    Call<Void> DeleteFav(@Part("id1") int p1,@Part("id2") int p2);
+    Call<Void> DeleteFav(@Part("id1") int p1,@Part("id2") String p2);
 
     @GET("employees/checkLogin/{login}")
     Call<Boolean> checkLogin(@Path("login") String login);
@@ -62,5 +65,14 @@ public interface ApiInterface {
 
     @Multipart
     @POST("/favList/addFav")
-    Call<Person> addFav(@Part("p1") int p1,@Part("p2") int p2);
+    Call<Person> addFav(@Part("p1") int p1,@Part("p2") String p2);
+
+    @POST("/employees/{id}/ratings")
+    Call<Employee> AddRating(@Body Rating rating, @Path("id") String id);
+
+    @GET("/employees/{id}/ratings")
+    Call<Float> SumRating(@Path("id") String id);
+
+    @PUT("/{id}/ratings/{id_rating}")
+    Call<Employee> updateRating(@Path("id") String id, @Path("id_rating") int id_rating, @Body Rating ratingRequest);
 }
